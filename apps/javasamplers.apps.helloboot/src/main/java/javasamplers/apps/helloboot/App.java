64afbb342506f2efd46afbe2aaeb5eb9fa825929
oceanebelle <oceanebelle@users.noreply.github.com>
2017-12-12 23:11:48 +0000

    skeleton multi module app

---
+++ apps/javasamplers.apps.helloboot/src/main/java/javasamplers/apps/helloboot/App.java
@@ -0,0 +1,13 @@
+package javasamplers.apps.helloboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
