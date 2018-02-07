4be3cb1223ad2589f130df0285bace0ded19823b
oceanebelle <oceanebelle@users.noreply.github.com>
2018-02-07 21:31:27 +0000

    create a spring boot and drop the angular prod built files in static

---
+++ angularspring/src/main/java/com/oceanebelle/javasamplers/angularspring/AngularSpringApp.java
@@ -0,0 +1,13 @@
+package com.oceanebelle.javasamplers.angularspring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringApp {
+
+    public static void main(String... args) {
+        SpringApplication.run(AngularSpringApp.class, args);
+    }
+}
