cc5e3f98175d29a21d84e5acea20744181a9e87d
oceanebelle <oceanebelle@users.noreply.github.com>
2018-02-09 22:44:18 +0000

    context load test

--- entities-jpa/src/test/java/com/oceanebelle/javasamplers/entitiessample/app/ContextLoadTest.java
+++ entities-jpa/src/test/java/com/oceanebelle/javasamplers/entitiessample/app/ContextLoadTest.java
@@ -4,6 +4,7 @@ import com.oceanebelle.javasamplers.entitiessample.dataaccess.NotebookDataServic
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +12,7 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class ContextLoadTest {
 
     @Autowired
